8e8febbb3a05e016d1f6c933fb92684ce90eb764
dplazas <dplazas@poligran.edu.co>
2025-09-18 12:59:33 -0500

    cambio launcher

--- src/main/java/co/edu/poli/aplicacion/vista/Launcher.java
+++ src/main/java/co/edu/poli/aplicacion/vista/Launcher.java
@@ -3,5 +3,7 @@ package co.edu.poli.aplicacion.vista;
 public class Launcher {
     public static void main(String[] args) {
         App.main(args); // Llama al main de tu clase App
+    System.out.println("hhahsa");
     }
+    
 }
